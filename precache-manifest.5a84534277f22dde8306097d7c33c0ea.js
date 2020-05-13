dcf28ca027498a4a9f2456d009f777db0c43b248
DavidKe91 <davidgkennedy@hotmail.com>
2020-05-13 23:54:05 +0100

    Updates

---
+++ precache-manifest.5a84534277f22dde8306097d7c33c0ea.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "444567569388fa7e797b",
+    "url": "/hacker-news-redux/static/js/main.44456756.chunk.js"
+  },
+  {
+    "revision": "27a5708b981cfc673735",
+    "url": "/hacker-news-redux/static/js/1.27a5708b.chunk.js"
+  },
+  {
+    "revision": "ca87e6a893e1d18cff6a",
+    "url": "/hacker-news-redux/static/js/runtime~main.ca87e6a8.js"
+  },
+  {
+    "revision": "3c1c0d24c6b9be0a32c5647da685422c",
+    "url": "/hacker-news-redux/index.html"
+  }
+];
